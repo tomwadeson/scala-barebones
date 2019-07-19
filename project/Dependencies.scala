f643ca8b5ab392ba6b010a3133ba123ccbf48249
Scala Steward <me@scala-steward.org>
2019-07-19 17:58:20 +0200

    Update better-monadic-for to 0.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   
   object CompilerPlugins {
     val kindProjector = compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
-    val betterMonadicFor = compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.0")
+    val betterMonadicFor = compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
     val silencer = compilerPlugin("com.github.ghik" %% "silencer-plugin" % silencerVersion)
   }
 }
