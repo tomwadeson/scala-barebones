bf1af9747e0f3ffa88ff73d97362dea022be1908
Tom Wadeson <tom@tomwadeson.com>
2019-07-30 19:00:47 +0100

    Add dependency on `cats-core`

--- build.sbt
+++ build.sbt
@@ -1,18 +1,16 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.0"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
-ThisBuild / organizationName := "example"
+ThisBuild / scalaVersion        := "2.13.0"
+ThisBuild / version             := "0.1.0-SNAPSHOT"
+ThisBuild / organization        := "com.example"
+ThisBuild / organizationName    := "example"
+ThisBuild / libraryDependencies ++= CompilerPlugins.all
 
 lazy val root = (project in file("."))
   .settings(
     name := "scala-barebones",
     libraryDependencies ++= Seq(
-      CompilerPlugins.betterMonadicFor,
-      CompilerPlugins.kindProjector,
-      CompilerPlugins.silencer,
-      silencerLib % Provided,
+      catsCore,
       scalaTest % Test
     )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,28 @@ import sbt._
 
 object Dependencies {
 
-  val silencerVersion = "1.4.1"
-
-  val silencerLib = "com.github.ghik" %% "silencer-lib" % silencerVersion 
+  val catsCore = "org.typelevel" %% "cats-core" % "2.0.0-M4"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   
   object CompilerPlugins {
-    val kindProjector = compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
-    val betterMonadicFor = compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
-    val silencer = compilerPlugin("com.github.ghik" %% "silencer-plugin" % silencerVersion)
+    val kindProjector =
+      compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
+
+    val betterMonadicFor =
+      compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+
+    val silencer = {
+      val version = "1.4.1"
+      Seq(
+        compilerPlugin("com.github.ghik" %% "silencer-plugin" % version),
+        "com.github.ghik" %% "silencer-lib" % version % Provided
+      )
+    }
+
+    val all =
+      Seq(
+        kindProjector,
+        betterMonadicFor
+      ) ++ silencer
   }
 }
