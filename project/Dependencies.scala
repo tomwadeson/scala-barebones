63539ee346f822c07734de90294b2c1e8bd80733
Scala Steward <me@scala-steward.org>
2020-11-26 10:15:10 +0100

    Update cats-core to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
+  val catsCore = "org.typelevel" %% "cats-core" % "2.3.0"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
   
   object CompilerPlugins {
