c459d64052ba6ae3c18a5cc9ad9b85474f434b81
Tom Wadeson <tom@tomwadeson.com>
2021-11-09 10:42:45 +0000

    Remove silencer plugin

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,26 +4,19 @@ object Dependencies {
 
   val catsCore = "org.typelevel" %% "cats-core" % "2.6.1"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.1.4"
-  
+
   object CompilerPlugins {
+
     val kindProjector =
       compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
 
     val betterMonadicFor =
       compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
 
-    val silencer = {
-      val version = "1.4.2"
-      Seq(
-        compilerPlugin("com.github.ghik" %% "silencer-plugin" % version),
-        "com.github.ghik" %% "silencer-lib" % version % Provided
-      )
-    }
-
     val all =
       Seq(
         kindProjector,
         betterMonadicFor
-      ) ++ silencer
+      )
   }
 }
